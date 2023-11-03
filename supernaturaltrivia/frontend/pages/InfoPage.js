bf889a28895fe27779c19531d76f6f3b79f0015a
valentinnistoran <valentin.nistoran@student.upt.ro>
2023-11-03 22:17:33 +0200

    Restructured the project
    Created a backend folder which holds the .NET project, and a frontend folder which holds the react project

---
+++ supernaturaltrivia/frontend/pages/InfoPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+function InfoPage() {
+  return (
+    <View>
+      <Text>Info Page</Text>
+      {/* Add content for the Info screen */}
+    </View>
+  );
+}
+
+export default InfoPage;
