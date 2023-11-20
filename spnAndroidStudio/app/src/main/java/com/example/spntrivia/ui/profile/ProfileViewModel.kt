0c35fea4500a1d062a0a12a89b9e720af1c0a93f
valentinnistoran <valentin.nistoran@student.upt.ro>
2023-11-20 16:03:17 +0200

    Updated from Notifications to Profile Fragment

--- spnAndroidStudio/app/src/main/java/com/example/spntrivia/ui/profile/ProfileViewModel.kt
+++ spnAndroidStudio/app/src/main/java/com/example/spntrivia/ui/profile/ProfileViewModel.kt
@@ -1,13 +1,14 @@
-package com.example.spntrivia.ui.notifications
+package com.example.spntrivia.ui.profile
+
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class NotificationsViewModel : ViewModel() {
+class ProfileViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
+        value = "This is profile Fragment"
     }
     val text: LiveData<String> = _text
 }
