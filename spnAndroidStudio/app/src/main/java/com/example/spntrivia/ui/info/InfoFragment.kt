f99db400d6db01e689c69861f90afe3755927512
sori <sorinapopaan@gmail.com>
2023-11-25 00:12:16 +0200

    Changed Info, Profile, and Settings to viewModel

--- spnAndroidStudio/app/src/main/java/com/example/spntrivia/ui/info/InfoFragment.kt
+++ spnAndroidStudio/app/src/main/java/com/example/spntrivia/ui/info/InfoFragment.kt
@@ -4,39 +4,25 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import androidx.fragment.app.viewModels
 import com.example.spntrivia.databinding.FragmentInfoBinding
 
 class InfoFragment : Fragment() {
 
-    private var _binding: FragmentInfoBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
+    private lateinit var binding: FragmentInfoBinding
+    private val infoViewModel: InfoViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val dashboardViewModel =
-            ViewModelProvider(this).get(InfoViewModel::class.java)
 
-        _binding = FragmentInfoBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+        binding = FragmentInfoBinding.inflate(inflater, container, false)
+        binding.lifecycleOwner = viewLifecycleOwner
+        binding.infoViewModel = infoViewModel
 
-        val textView: TextView = binding.textInfo
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        return root
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+        return binding.root
     }
 }
--- spnAndroidStudio/app/src/main/java/com/example/spntrivia/ui/profile/ProfileFragment.kt
+++ spnAndroidStudio/app/src/main/java/com/example/spntrivia/ui/profile/ProfileFragment.kt
@@ -4,39 +4,25 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import androidx.fragment.app.viewModels
 import com.example.spntrivia.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
 
-    private var _binding: FragmentProfileBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
+    private lateinit var binding: FragmentProfileBinding
+    private val profileViewModel: ProfileViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val notificationsViewModel =
-            ViewModelProvider(this).get(ProfileViewModel::class.java)
 
-        _binding = FragmentProfileBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+        binding = FragmentProfileBinding.inflate(inflater, container, false)
+        binding.lifecycleOwner = viewLifecycleOwner
+        binding.profileViewModel = profileViewModel
 
-        val textView: TextView = binding.textProfile
-        notificationsViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        return root
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+        return binding.root
     }
 }
