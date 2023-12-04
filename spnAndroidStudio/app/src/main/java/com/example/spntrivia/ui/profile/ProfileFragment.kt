db784af43466ff73eca172372201d86855fb1e4e
valentinnistoran <valentin.nistoran@student.upt.ro>
2023-12-04 22:36:27 +0200

    Update Picture commit

--- spnAndroidStudio/app/src/main/java/com/example/spntrivia/ui/profile/ProfileFragment.kt
+++ spnAndroidStudio/app/src/main/java/com/example/spntrivia/ui/profile/ProfileFragment.kt
@@ -1,5 +1,8 @@
 package com.example.spntrivia.ui.profile
 
+import android.app.Activity
+import android.content.Intent
+import android.provider.MediaStore
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -18,11 +21,33 @@ class ProfileFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-
         binding = FragmentProfileBinding.inflate(inflater, container, false)
         binding.lifecycleOwner = viewLifecycleOwner
         binding.profileViewModel = profileViewModel
 
+        binding.profileImage.setOnClickListener {
+            openImagePicker()
+        }
+
         return binding.root
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == Activity.RESULT_OK && data != null) {
+            val selectedImageUri = data.data
+            binding.profileImage.setImageURI(selectedImageUri)
+        }
+    }
+
+    private fun openImagePicker() {
+        val galleryIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        startActivityForResult(galleryIntent, PICK_IMAGE_REQUEST)
+    }
+
+
+    companion object {
+        private const val PICK_IMAGE_REQUEST = 1
+    }
+
 }
