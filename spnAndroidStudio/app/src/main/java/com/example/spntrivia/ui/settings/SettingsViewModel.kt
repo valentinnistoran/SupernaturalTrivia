8bcff3f3c2b74f7608bf29b1f437c6c5545eb2ed
valentinnistoran <valentin.nistoran@student.upt.ro>
2023-12-28 15:38:26 +0200

    Finished the settings page implementation and reformatted the json files

--- spnAndroidStudio/app/src/main/java/com/example/spntrivia/ui/settings/SettingsViewModel.kt
+++ spnAndroidStudio/app/src/main/java/com/example/spntrivia/ui/settings/SettingsViewModel.kt
@@ -5,8 +5,25 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class SettingsViewModel : ViewModel() {
+
+    val onHelpButtonClicked = MutableLiveData(false)
+    val onAboutUsButtonClicked = MutableLiveData(false)
+    val onShareButtonClicked = MutableLiveData(false)
+
     private val _text = MutableLiveData<String>().apply {
         value = "This is settings Fragment"
     }
     val text: LiveData<String> = _text
+
+    fun onClickHelpButton(){
+        onHelpButtonClicked.value=true
+    }
+
+    fun onClickAboutUsButton(){
+        onAboutUsButtonClicked.value=true
+    }
+
+    fun onClickShareButton(){
+        onShareButtonClicked.value=true
+    }
 }
