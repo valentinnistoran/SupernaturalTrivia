fdedfadd28dbcaa7e6dedabb849105a2065cca73
sori <sorinapopaan@gmail.com>
2023-12-28 14:16:26 +0200

    Updated the Trivia Logic: Not Working

--- spnAndroidStudio/app/src/main/java/com/example/spntrivia/ui/trivia/TriviaFragment.kt
+++ spnAndroidStudio/app/src/main/java/com/example/spntrivia/ui/trivia/TriviaFragment.kt
@@ -1,12 +1,15 @@
 package com.example.spntrivia.ui.trivia
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import com.example.spntrivia.R
 import com.example.spntrivia.databinding.FragmentTriviaBinding
+
 class TriviaFragment : Fragment() {
 
     private lateinit var binding: FragmentTriviaBinding
@@ -24,6 +27,26 @@ class TriviaFragment : Fragment() {
         binding.triviaViewModel = triviaViewModel
         return binding.root
 
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val sharedPreferences = activity!!.getSharedPreferences(
+            getString(R.string.shared_preference_game_mode), Context.MODE_PRIVATE
+        )
+        val triviaViewModel: TriviaViewModel by viewModels()
+
+        val difficulty = sharedPreferences.getInt(getString(R.string.game_mode_key), 1)
+
+        triviaViewModel.getQuestionsByDifficulty(difficulty)
+
+        // Observe the questions and update the UI accordingly
+        triviaViewModel.questions.observe(viewLifecycleOwner) { questions ->
+            // Update UI with the questions
+            // For example, set text for quiz_question TextView
+            binding.quizQuestion.text = questions.firstOrNull()?.questionText ?: ""
+        }
     }
 
 }
--- spnAndroidStudio/app/src/main/java/com/example/spntrivia/ui/trivia/TriviaViewModel.kt
+++ spnAndroidStudio/app/src/main/java/com/example/spntrivia/ui/trivia/TriviaViewModel.kt
@@ -1,7 +1,25 @@
 package com.example.spntrivia.ui.trivia
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.spntrivia.repository.TriviaRepository
+import com.example.spntrivia.triviaDB.Question
+import kotlinx.coroutines.launch
 
-class TriviaViewModel : ViewModel() {
+class TriviaViewModel(private val triviaRepository: TriviaRepository) : ViewModel() {
+    private val _questions = MutableLiveData<List<Question>>()
+    val questions: LiveData<List<Question>> get() = _questions
 
+    fun getQuestionsByDifficulty(difficulty: Int) {
+        viewModelScope.launch {
+            _questions.value = when (difficulty) {
+                1 -> triviaRepository.getAllEasyQuestions()
+                2 -> triviaRepository.getAllMediumQuestions()
+                3 -> triviaRepository.getAllHardQuestions()
+                else -> emptyList()
+            }
+        }
+    }
 }
