e2ae49ee3943883dadd516f0792e5efd2032cc7d
valentinnistoran <valentin.nistoran@student.upt.ro>
2024-01-04 15:14:09 +0200

    end quiz display according to rank

--- spnAndroidStudio/app/src/main/java/com/example/spntrivia/ui/endQuiz/EndQuizViewModel.kt
+++ spnAndroidStudio/app/src/main/java/com/example/spntrivia/ui/endQuiz/EndQuizViewModel.kt
@@ -1,8 +1,12 @@
 package com.example.spntrivia.ui.endQuiz
 
+import android.content.Context
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.spntrivia.gameHistoryDB.QuizResult
+import com.example.spntrivia.ui.endQuiz.business.Ranking
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 
 class EndQuizViewModel : ViewModel() {
 
@@ -13,6 +17,9 @@ class EndQuizViewModel : ViewModel() {
     val calculatedRank = MutableLiveData(0)
 
     val lastQuizResult = MutableLiveData<QuizResult?>()
+    var ranksList: List<Ranking>? = null
+    var rank: Ranking? = null
+
     fun observeLastQuizResult(quizResult: QuizResult?) {
         lastQuizResult.value = quizResult
         quizResult?.let {
@@ -24,6 +31,15 @@ class EndQuizViewModel : ViewModel() {
 
     fun onClickBackHomeButton() {
         onBackHomeButtonClicked.value = true
+    }
+
+    fun loadRanks(context: Context) {
+        val json: String = context.assets.open("ranking.json").bufferedReader().use {
+            it.readText()
+        }
+
+        val rankingListType = object : TypeToken<List<Ranking>>() {}.type
+        ranksList = Gson().fromJson(json, rankingListType)
     }
 
 }
